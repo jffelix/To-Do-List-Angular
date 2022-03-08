428b949df1b746ec2cb867780256d5bad84e0d8c
Jean-Luc Felix <jean-luc-mac@jean-lucs-mbp.lan>
2022-03-07 19:34:31 -0800

    Rendered list of groceries

--- to-do-list/src/app/app.module.ts
+++ to-do-list/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GroceryListComponent } from './groceries/grocery-list.component';
+import { GroceryListService } from './groceries/grocery-list.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { GroceryListComponent } from './groceries/grocery-list.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [GroceryListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- to-do-list/src/app/groceries/grocery-list.component.ts
+++ to-do-list/src/app/groceries/grocery-list.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { GroceryListService } from './grocery-list.service';
 
 @Component({
     selector: 'grocery-list',
-    template: '<h2>Grocery List</h2>'
+    template: `
+        <h2>Grocery List</h2>
+        <ul *ngFor="let grocery of groceries">
+            <h3>{{ grocery.item }}</h3>
+            <p>{{ grocery.quantity }}</p>
+        </ul>
+    `
 })
 
 export class GroceryListComponent {
+    title = "List of groceries";
+    groceries;
 
+    constructor(service: GroceryListService) {
+        this.groceries = service.getGroceries();
+    }
+
+    testOnClick() {
+        console.log("You clicked button!");
+    }
 }
