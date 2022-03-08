4be9684f2c9d76914691f32ff2646fad5b114e88
Jean-Luc Felix <jean-luc-mac@jean-lucs-mbp.lan>
2022-03-07 19:26:14 -0800

    Created grocery-list.service.ts

---
+++ to-do-list/src/app/groceries/grocery-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'grocery-list',
+    template: '<h2>Grocery List</h2>'
+})
+
+export class GroceryListComponent {
+
+}
---
+++ to-do-list/src/app/groceries/grocery-list.service.ts
@@ -0,0 +1,19 @@
+
+export class GroceryListService {
+    getGroceries() {
+        return [
+            {
+                item: "apples",
+                quantity: 4
+            },
+            {
+                item: "strawberries",
+                quantity: 2
+            },
+            {
+                item: "bananas",
+                quantity: 5
+            }
+        ]
+    }
+}
