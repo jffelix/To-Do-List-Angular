31b7405d0cf2a9664ddbf938c84634e88cd67b7a
Jean-Luc Felix <jean-luc-mac@jean-lucs-mbp.lan>
2022-03-07 20:19:08 -0800

    Created methods for input fields

--- to-do-list/src/app/app.component.ts
+++ to-do-list/src/app/app.component.ts
@@ -7,8 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'To-Do List (Angular)';
-
-  addItem() {
-    alert("You clicked the Submit button!");
-  }
 }
--- to-do-list/src/app/inputForm/input-form.component.ts
+++ to-do-list/src/app/inputForm/input-form.component.ts
@@ -5,12 +5,35 @@ import { Component } from '@angular/core';
     template: `
         <h3>Add new item</h3>
         <p>Name</p>
-        <input />
+        <input 
+            type="text" 
+            #box
+            (keyup)="getNameInput(box.value)"
+            placeholder="ex: oranges"
+        />
         <p>Quantity</p>
-        <input />
+        <input 
+            type="text" 
+            #box
+            (keyup)="getQuantityInput(box.value)"
+            placeholder="ex: 3"
+        />
+        <button (click)="addItem()">Submit</button>
     `
 })
 
 export class InputFormComponent {
+    title = "Input form to add item";
 
+    getNameInput(name: string) {
+        console.log("name: ", name);
+    }
+
+    getQuantityInput(quantity: string) {
+        console.log("quantity: ", quantity);
+    }
+
+    addItem() {
+        alert("You clicked the Submit button!");
+    }
 }
